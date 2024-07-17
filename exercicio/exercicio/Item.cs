b6c5516d10db8022b1d91cef44d96f70d5e4aeb8
Jonathan Iha <58711422+JapaTech@users.noreply.github.com>
2024-07-17 19:22:11 -0300

    Manipulação de Arquivos

---
+++ exercicio/exercicio/Item.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio
+{
+    internal class Item
+    {
+        public string Nome { get; set; }
+        public float Preco { get; set; }
+        public int Quantidade { get; set; }
+
+        public Item(string nome, float preco, int quantidade) 
+        { 
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }   
+
+        public float ValorTotal()
+        {
+            return Preco * Quantidade;
+        }
+    }
+}
---
+++ exercicio/exercicio/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Net.Http.Headers;
+
+namespace exercicio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Pede para o usuário escrever o local do csv
+            Console.WriteLine("Escreva o caminho do arquivo: ");
+            string sourcepath = Console.ReadLine(); 
+
+            try
+            {
+                //Lê todas as linhas do arquivo do usuário
+                string[] lines = File.ReadAllLines(sourcepath);
+
+                //Guarda a pasta do usuário
+                string sourceFolderPath = Path.GetDirectoryName(sourcepath);
+                //Cria o caminho para uma nova pasta
+                string targetFolderPath = sourceFolderPath + @"\out";
+                //Cria o caminho para o novo arquivo
+                string targetFilePath = targetFolderPath + @"\summary.csv";
+
+                //Cria a pasta nova
+                Directory.CreateDirectory(targetFolderPath);
+
+                using (StreamWriter sw =  File.AppendText(targetFilePath)) 
+                {
+                    //Para cada linha encontrada
+                    foreach (string line in lines) 
+                    {
+                        //Extrai as informações
+                        string[] fields = line.Split(',');
+                        string name = fields[0];
+                        float value = float.Parse(fields[1], System.Globalization.CultureInfo.InvariantCulture);
+                        int qtd = int.Parse(fields[2]);
+
+                        //Cria um produto
+                        Item i = new Item(name, value, qtd);
+
+                        //Escreve num arquivo o nome do protudo e o valor total
+                        sw.WriteLine(i.Nome + "," + i.ValorTotal().ToString("F2", System.Globalization.CultureInfo.InvariantCulture));
+                    }
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
